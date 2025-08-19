c22cd05b359f1f4e5ee63c68a9a7329fb6abdc63
Brian Pennington <srsrec@gmail.com>
2025-08-19 13:19:29 -0500

    Loops exercise complete

--- exercise-loops/src/ForLoops.java
+++ exercise-loops/src/ForLoops.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
+
 public class ForLoops {
     public static void main(String[] args) {
-
+        Scanner console = new Scanner(System.in);
     //1. Count Up! (For Loop)
     //● Write a program that prints numbers 1 to 100 using a for loop.
     //● Modify the program to print only even numbers.
+        for (int i = 2; i <= 100; i+=2) {
+            System.out.println(i);
+        }
 
     //2.Multiplication Table (For Loop)
     //● Ask the user for a number.
     //● Print the multiplication table (1 to 10) for that number
+        System.out.println("Enter a number: ");
+        int enteredNumber = Integer.parseInt(console.nextLine());
 
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(enteredNumber + " times " + i + " = " + (enteredNumber * i));
+        }
     }
 }
--- exercise-loops/src/WhileLoops.java
+++ exercise-loops/src/WhileLoops.java
@@ -1,14 +1,33 @@
+import java.util.Scanner;
+
 public class WhileLoops {
     public static void main(String[] args) {
-
+        Scanner console = new Scanner(System.in);
         //1. Countdown Timer (While Loop)
         //● Prompt the user for a starting number.
         //● Use a while loop to count down to zero.
         //● Print "Blast off!" when it reaches zero
+        System.out.println("Enter a starting number: ");
+        int startingNumber = Integer.parseInt(console.nextLine());
 
+        while (startingNumber > 0) {
+            System.out.println("Counter.. " + startingNumber + "!");
+            startingNumber--;
+        }
+        System.out.println("BLAST OFF!!");
         //2. Password Validator (While Loop)
         //● Ask the user to enter a password.
         //● Keep asking until they enter the correct password ("letmein").
 
+        Boolean password = false;
+
+        while (!password) {
+            System.out.println("Please enter your password: ");
+            String passAttempt = console.nextLine();
+            if (passAttempt.equals("letmein")) {
+                password = true;
+            }
+        }
+        System.out.println("Now logged in.");
     }
 }
