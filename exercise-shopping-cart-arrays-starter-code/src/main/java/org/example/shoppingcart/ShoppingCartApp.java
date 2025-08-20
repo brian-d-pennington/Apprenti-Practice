55345f5f784735e4504b216ff0ceaf3f2145a664
Brian Pennington <srsrec@gmail.com>
2025-08-20 16:06:36 -0500

    save unstaged changes

--- exercise-shopping-cart-arrays-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
+++ exercise-shopping-cart-arrays-starter-code/src/main/java/org/example/shoppingcart/ShoppingCartApp.java
@@ -9,9 +9,21 @@ public class ShoppingCartApp {
     System.out.println("Welcome to the shopping cart app!");
 
     // Create arrays to contain addresses and sizes
+    String[] addresses = new String[4];
+    addresses[0] = "2345 Meadow Dr.";
+    addresses[1] = "Dandelion Road";
+    addresses[2] = "777 Pine Way";
+    addresses[3] = "632 Book Lane";
+
+    String[] sizes = new String[3];
+    sizes[0] = "small";
+    sizes[1] = "medium";
+    sizes[2] = "large";
 
     // Prompt for shipping address
-
+    for (String address : addresses) {
+        System.out.println(address);
+    }
     // Prompt for Size
 
     // Print details
