be51af949468ebce74374b757948f5d560dd4f8b
Brian Pennington <44206137+brian-d-pennington@users.noreply.github.com>
2025-08-26 14:03:24 -0500

    AquariumTracker exercise complete

--- java-oop/exercise-classes-and-objects/AquariumTracker/src/main/java/AquariumFish.java
+++ java-oop/exercise-classes-and-objects/AquariumTracker/src/main/java/AquariumFish.java
@@ -1,4 +1,63 @@
 public class AquariumFish {
+    // local variables
+    private String species;
+    private String commonName;
+    private double maxTemp;
+    private double minTemp;
+    private String diet;
+    // constructor
+    public AquariumFish(String species, String commonName, double maxTemp, double minTemp, String diet) {
+        this.species = species;
+        this. commonName = commonName;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+        this.diet = diet;
+    }
+    // getters and setters
 
+    public String getSpecies() {
+        return species;
+    }
 
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(double maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(double minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public String getDiet() {
+        return diet;
+    }
+
+    public void setDiet(String diet) {
+        this.diet = diet;
+    }
+
+    // methods
+
+    public double averageTemp() {
+        return (minTemp + maxTemp) / 2;
+    }
 }
--- java-oop/exercise-classes-and-objects/AquariumTracker/src/main/java/AquariumTracker.java
+++ java-oop/exercise-classes-and-objects/AquariumTracker/src/main/java/AquariumTracker.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class AquariumTracker {
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        AquariumFish aquariumFish = new AquariumFish("Philum Mobulidae", "Devil Fish", 15.5, 29.0, "Plankton and krill" );
+        // print out fish attributes and average temperature
+        System.out.println("Species of fish : " + aquariumFish.getSpecies());
+        System.out.println("Common name : " + aquariumFish.getCommonName());
+        System.out.println("Maximum temp in C : " + aquariumFish.getMaxTemp());
+        System.out.println("Minimum temp in C : " + aquariumFish.getMinTemp());
+        System.out.println("Diet : " + aquariumFish.getDiet());
+        System.out.println("Average Temp = " + aquariumFish.averageTemp());
     }
 }
