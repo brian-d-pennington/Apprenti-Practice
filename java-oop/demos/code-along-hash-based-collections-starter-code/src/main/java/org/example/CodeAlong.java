863d305b2e093fe5d549dcaf40d5853a48fe8e1c
Brian Pennington <44206137+brian-d-pennington@users.noreply.github.com>
2025-09-08 15:45:05 -0500

    push HashMap code along demo

---
+++ java-oop/demos/code-along-hash-based-collections-starter-code/src/main/java/org/example/CodeAlong.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class CodeAlong {
+    public static void main(String[] args) {
+        BirthdayManager birthday = new BirthdayManager();
+
+        Friend ralph = new Friend("Ralph", "05/19/1981");
+        birthday.addFriend(ralph);
+
+        Friend pedro = new Friend("Pedro", "06/16/1916");
+        birthday.addFriend(pedro);
+
+        for (Friend friend: birthday.getFriends()) {
+            System.out.println(friend);
+        }
+
+        for (Friend friend : birthday.getMap().keySet()) {
+            
+        }
+    }
+}
