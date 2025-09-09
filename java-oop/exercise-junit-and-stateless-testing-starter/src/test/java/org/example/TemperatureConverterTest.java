1e41a403717d2c8eb0ebdf5eb2b82b53891a4e37
Brian Pennington <44206137+brian-d-pennington@users.noreply.github.com>
2025-09-09 14:00:11 -0500

    completed Stateless Test exercise

--- java-oop/exercise-junit-and-stateless-testing-starter/src/test/java/org/example/TemperatureConverterTest.java
+++ java-oop/exercise-junit-and-stateless-testing-starter/src/test/java/org/example/TemperatureConverterTest.java
@@ -1,9 +1,116 @@
 package org.example;
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TemperatureConverterTest {
     private TemperatureConverter converter;
+
+    // set up
+    @BeforeEach
+    void setUp() {
+        converter = new TemperatureConverter();
+    }
+
+    // test
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        // arrange
+        double expected = 73.4;
+        // act
+        double actual = converter.celsiusToFahrenheit(23);
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFahrenheitToCelsius() {
+        // arrange
+        double expected = 35;
+        // act
+        double actual = converter.fahrenheitToCelsius(95);
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFahrenheitToKelvin() {
+        // arrange
+        double expected = 293;
+        // act
+        double actual = converter.fahrenheitToKelvin(67.73);
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCelsiusToKelvin() {
+        // arrange
+        double expected = 291;
+        // act
+        double actual = converter.celsiusToKelvin(17.85);
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testKelvinToCelsius() {
+        // arrange
+        double expected = 27.85;
+        // act
+        double actual = converter.kelvinToCelsius(301);
+        BigDecimal bd = new BigDecimal(actual);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        actual = bd.doubleValue();
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testKelvinToFahrenheit() {
+        // arrange
+        double expected = 1.13;
+        // act
+        double actual = converter.kelvinToFahrenheit(256);
+        BigDecimal bd = new BigDecimal(actual);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        actual = bd.doubleValue();
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testKelvinToFahrenheitNegativeInput() {
+        // arrange
+        double expected = -1; // error exception
+        // act
+        double actual = converter.kelvinToFahrenheit(-10.0);
+        BigDecimal bd = new BigDecimal(actual);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        actual = bd.doubleValue();
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testKelvinToCelsiusNegativeInput() {
+        // arrange
+        double expected = -1; // error exception
+        // act
+        double actual = converter.kelvinToCelsius(-25);
+        BigDecimal bd = new BigDecimal(actual);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        actual = bd.doubleValue();
+        // assert
+        assertEquals(expected, actual);
+    }
 
 
 }
