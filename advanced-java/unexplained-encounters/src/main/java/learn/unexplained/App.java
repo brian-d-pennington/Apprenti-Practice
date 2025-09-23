eb83a9d5db3425ec6a9f10fc0c7ddcd99769769b
Brian Pennington <44206137+brian-d-pennington@users.noreply.github.com>
2025-09-23 09:59:42 -0500

    successfully added beans to Spring Dependency

--- advanced-java/unexplained-encounters/src/main/java/learn/unexplained/App.java
+++ advanced-java/unexplained-encounters/src/main/java/learn/unexplained/App.java
@@ -2,17 +2,21 @@ package learn.unexplained;
 
 import learn.unexplained.data.EncounterFileRepository;
 import learn.unexplained.domain.EncounterService;
+import learn.unexplained.models.Encounter;
 import learn.unexplained.ui.Controller;
 import learn.unexplained.ui.View;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
-        EncounterFileRepository repository = new EncounterFileRepository("./data/encounters.csv");
-        EncounterService encounterService = new EncounterService(repository);
-        View view = new View();
-        Controller controller = new Controller(encounterService, view);
-
+//        EncounterFileRepository repository = new EncounterFileRepository("./data/encounters.csv");
+//        EncounterService encounterService = new EncounterService(repository);
+//        View view = new View();
+//        Controller controller = new Controller(encounterService, view);
+        ApplicationContext context = new ClassPathXmlApplicationContext("dependency-configuration.xml");
+        Controller controller = context.getBean(Controller.class);
         controller.run();
     }
 }
