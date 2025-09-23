88e204f7444777fb9a7c8f09405def1b5a8903cf
Brian Pennington <44206137+brian-d-pennington@users.noreply.github.com>
2025-09-23 11:42:23 -0500

    Encounters Spring annotations complete

--- advanced-java/unexplained-encounters/src/main/java/learn/unexplained/App.java
+++ advanced-java/unexplained-encounters/src/main/java/learn/unexplained/App.java
@@ -4,13 +4,11 @@ import learn.unexplained.ui.Controller;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @ComponentScan
 public class App {
 
     public static void main(String[] args) {
-//        ApplicationContext context = new ClassPathXmlApplicationContext("dependency-configuration.xml");
         ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         Controller controller = context.getBean(Controller.class);
         controller.run();
